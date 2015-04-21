795e8da905f5613c8d14ae69805b558f845a57f6
Derek DiCillo <dddicillo@gmail.com>
2015-04-21 10:21:14 -0400

    Add some API endpoints and SharedPreferences

--- app/src/main/java/com/derekdicillo/fileserver/FileAPI.java
+++ app/src/main/java/com/derekdicillo/fileserver/FileAPI.java
@@ -1,23 +1,24 @@
 package com.derekdicillo.fileserver;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.util.Log;
-import android.util.LruCache;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -25,35 +26,23 @@ import java.util.Map;
  */
 public class FileAPI {
 
+    public static final String API_PREFS = "api_prefs";
+    public static final String USER_ID = "user_id";
+    public static final String ACCESS_TOKEN = "access_token";
     private static final String TAG = "FileAPI";
-
     // TODO Replace with correct base url
-    private static final String BASE_URL = "http://192.168.1.25:3000/explorer/resources/";
-
+    private static final String BASE_URL = "http://192.168.1.25:3000/api/";
     private static FileAPI mInstance;
     private static Context mCtx;
     private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
+    private DownloadManager mDownloadManager;
+    private SharedPreferences mPrefs;
 
     private FileAPI(Context context) {
         mCtx = context;
         mRequestQueue = getRequestQueue();
-
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
+        mDownloadManager = (DownloadManager) mCtx.getSystemService(Context.DOWNLOAD_SERVICE);
+        mPrefs = mCtx.getSharedPreferences(API_PREFS, Context.MODE_PRIVATE);
     }
 
     public static synchronized FileAPI getInstance(Context context) {
@@ -61,66 +50,6 @@ public class FileAPI {
             mInstance = new FileAPI(context);
         }
         return mInstance;
-    }
-
-    private RequestQueue getRequestQueue() {
-        if (mRequestQueue == null) {
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
-        }
-        return mRequestQueue;
-    }
-
-    private <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
-    }
-
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
-    }
-
-    /**
-     * List of names and ids of files available on server
-     *
-     * @param listener      callback on success
-     * @param errorListener callback on error
-     */
-    public void fileIndex(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-        execute("files", Request.Method.GET, null, listener, errorListener);
-    }
-
-    /**
-     * Download file with specified id
-     *
-     * @param fileId        id of file to be downloaded
-     * @param listener      callback on success
-     * @param errorListener callback on error
-     */
-    public void fileDownload(int fileId, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-        String url = String.format("file/%s", fileId);
-        execute(url, Request.Method.GET, null, listener, errorListener);
-    }
-
-    public void fileUpload(String fileName, File file, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-
-    }
-
-    private void execute(
-            String endpoint,
-            int method,
-            Map<String, String> params,
-            Response.Listener<JSONObject> listener,
-            Response.ErrorListener errorListener) {
-
-        JSONObject parameters = null;
-        if (params != null) {
-            parameters = new JSONObject(params);
-        }
-
-        JsonObjectRequest request = new JsonObjectRequest(method, BASE_URL + endpoint, parameters, listener, errorListener);
-
-        mRequestQueue.add(request);
     }
 
     public static boolean handleNetworkError(Activity context, VolleyError error) {
@@ -142,5 +71,88 @@ public class FileAPI {
             );
             return false;
         }
+    }
+
+    private RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    private <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    /**
+     * Retrieve userId and accessToken for user with specified email and password
+     *
+     * @param email         the user's email address
+     * @param password      the user's password
+     * @param listener      callback on success
+     * @param errorListener callback on error
+     */
+    public void login(String email, String password, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        execute("mUsers/login", Request.Method.POST, params, listener, errorListener);
+    }
+
+    /**
+     * Clears memory of current user locally and on the server-side
+     *
+     * @param listener      callback on success
+     * @param errorListener callback on error
+     */
+    public void logout(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        execute("mUsers/logout", Request.Method.POST, null, listener, errorListener);
+        // TODO Remove userId and accessToken from SharedPreferences
+        mPrefs.edit().remove(USER_ID).remove(ACCESS_TOKEN);
+    }
+
+    /**
+     * List of names and ids of files available on server
+     *
+     * @param listener      callback on success
+     * @param errorListener callback on error
+     */
+    public void fileIndex(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        execute("files", Request.Method.GET, null, listener, errorListener);
+    }
+
+    /**
+     * Download file with specified id
+     *
+     * @param fileName name of file to be downloaded
+     */
+    public void fileDownload(String fileName) {
+        String url = String.format("Containers/%s/download-file/%s", mPrefs.getString(USER_ID, "empty"), fileName);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        mDownloadManager.enqueue(request);
+        // TODO Register BroadcastReceiver
+    }
+
+    public void fileUpload(String fileName, File file, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+
+    }
+
+    private void execute(
+            String endpoint,
+            int method,
+            Map<String, String> params,
+            Response.Listener<JSONObject> listener,
+            Response.ErrorListener errorListener) {
+
+        JSONObject parameters = null;
+        if (params != null) {
+            parameters = new JSONObject(params);
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(method, BASE_URL + endpoint, parameters, listener, errorListener);
+
+        mRequestQueue.add(request);
     }
 }
